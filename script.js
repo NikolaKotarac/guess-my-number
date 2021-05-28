3dc53c83eebe331d8a40d83f0d0105cf70dedaa6
Nikola Kotarac <84856980+nikolakotarac@users.noreply.github.com>
2021-05-28 14:44:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 'use strict';
-// document.querySelector('.again').addEventListener('click', function () {
-//   score = 20;
-//   secretNumber = Math.trunc(Math.random() * 20) + 1;
-//   // document.querySelector('.message').textContent = 'Start guessing...';
-//   displayMessage('Start guessing...');
-//   document.querySelector('.score').textContent = 20;
-//   document.querySelector('.number').textContent = '?';
-//   document.querySelector('.guess').value = '';
-//   document.querySelector('body').style.backgroundColor = '#222';
-//   document.querySelector('.number').style.width = '15rem';
-// });
-
 let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let highScore = 0;
